43272fb2222505eeb1da3deb92441c1d3571a46c
miraks <a@vldkn.net>
2017-10-27 21:15:55 +0300

    Update dependencies and add prettier

--- src/bus.js
+++ src/bus.js
@@ -5,12 +5,16 @@ module.exports = () => {
   const bus = dbus.sessionBus()
   const service = bus.getService('org.gnome.SettingsDaemon')
 
-  service.getInterface('/org/gnome/SettingsDaemon/MediaKeys', 'org.gnome.SettingsDaemon.MediaKeys', (error, intrface) => {
-    if (error) throw error
+  service.getInterface(
+    '/org/gnome/SettingsDaemon/MediaKeys',
+    'org.gnome.SettingsDaemon.MediaKeys',
+    (error, intrface) => {
+      if (error) throw error
 
-    intrface.GrabMediaPlayerKeys('firefox-media-control', 0)
-    intrface.addListener('MediaPlayerKeyPressed', (app, key) => {
-      emitter.emit('key', key)
-    })
-  })
+      intrface.GrabMediaPlayerKeys('firefox-media-control', 0)
+      intrface.addListener('MediaPlayerKeyPressed', (app, key) => {
+        emitter.emit('key', key)
+      })
+    }
+  )
 }
